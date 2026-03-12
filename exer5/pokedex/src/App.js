66056a7773e4acb7972319605245f60f9e819a69
Cizki <alvaradof010407@gmail.com>
2026-03-12 15:21:55 -0400

    add useEffect to fetch pokemon from pokeapi

--- exer5/pokedex/src/App.js
+++ exer5/pokedex/src/App.js
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+
+const URL = "https://pokeapi.co/api/v2/pokemon";
 
 function App() {
   const [dexNumber, setDexNumber] = useState(1);
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    fetch(`${URL}/${dexNumber}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+        console.log(data);
+      });
+  }, [dexNumber]);
 
   return (
     <div className="app">
       <h1>Exercise 5 - PokeDex!</h1>
-      <p>dex number: {dexNumber}</p>
+      {pokemon && <p>{pokemon.name}</p>}
     </div>
   );
 }

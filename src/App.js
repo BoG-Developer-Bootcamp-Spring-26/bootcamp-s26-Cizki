ee9a90554f9abcdea4436a26c2d362c60036b73d
Cizki <alvaradof010407@gmail.com>
2026-03-12 15:02:32 -0400

    use PokemonCard component in App

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
+
+const URL = "https://pokeapi.co/api/v2/pokemon";
+
+function App() {
+  const [dexNumber, setDexNumber] = useState(1);
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`${URL}/${dexNumber}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, [dexNumber]);
+
+  function handlePrev() {
+    if (dexNumber > 1) {
+      setDexNumber(dexNumber - 1);
+    }
+  }
+
+  function handleNext() {
+    setDexNumber(dexNumber + 1);
+  }
+
+  return (
+    <div className="app">
+      <h1>Exercise 5 - PokeDex!</h1>
+      <div>
+        <button onClick={handlePrev}>{"<"}</button>
+        <button onClick={handleNext}>{">"}</button>
+      </div>
+      {loading && <p>Loading...</p>}
+      {!loading && pokemon && <PokemonCard pokemon={pokemon} />}
+    </div>
+  );
+}
+
+export default App;
